1fc9f7281a618dae19565bd377157541eee09ca2
merajmn345 <98653773+merajmn345@users.noreply.github.com>
2022-04-08 15:31:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div id="root">
+        <p>I am learning React. My life is getting better.</p>
+      </div>
+    </>
+  );
+}
